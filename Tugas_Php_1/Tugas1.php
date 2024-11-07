fb297a029dbc69bd9fd656112bc2aa6e2851803c
DhiasCoder <diashabibi8@gmail.com>
2024-11-07 17:41:31 +0700

    push

--- Tugas_Php_1/Tugas1.php
+++ Tugas_Php_1/Tugas1.php
@@ -4,35 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Operasi Penjumlahan dan Pengurangan</title>
-    <link href="./style.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <center>
         <div class="container">
+            <div class="judul">
+                <h3>PROSES PERHITUNGAN DARI RUMUS BANGUN RUANG</h3>
+            </div>
             <div class="isi">
                 <form method="post">
-                    Angka 1: <input type="number" name="num1"><br><br>
-                    Angka 2: <input type="number" name="num2"><br><br>
-                    Angka 3: <input type="number" name="num3"><br><br>
+                    <b>ANGKA 1: <input type="number" name="num1"><br><br></b>
+                    <b>ANGKA 2: <input type="number" name="num2"><br><br></b>
+                    <b>ANGKA 3: <input type="number" name="num3"><br><br></b>
                     <div class="nav-operator">
                         <button class="button" type="button">
                             OPERATOR
                         </button>
                         <ul class="drop-down">
                             <li><button type="submit" name="operation" value="Kubus">Luas Permukaan Kubus</button></li>
-                            <li><button type="submit" name="operation" value="Balok">Luas Permukaan Balok</button></li>
-                            <li><button type="submit" name="operation" value="Prisma_Segitiga">Volume Prisma Segitiga</button></li>
-                            <li><button type="submit" name="operation" value="Limas_Segiempat">Volume Limas Segiempat</button></li>
-                            <li><button type="submit" name="operation" value="Tabung">Volume Tabung</button></li>
-                            <li><button type="submit" name="operation" value="Kerucut">Luas Permukaan Kerucut</button></li>
-                            <li><button type="submit" name="operation" value="Bola">Luas Permukaan Bola</button></li>
+                            <li><button type="submit" name="operation" value="Balok">Luas Permukaan Balok <br> L=2 × (p × l + p × t + l × t)</button></li>
+                            <li><button type="submit" name="operation" value="Prisma_Segitiga">Volume Prisma Segitiga <br> V = (1/2 × Alas Segitiga × Tinggi Segitiga) × Tinggi Prisma</button></li>
+                            <li><button type="submit" name="operation" value="Limas_Segiempat">Volume Limas Segiempat <br> V = 1/3 × (Panjang × Lebar) × Tinggi</button></li>
+                            <li><button type="submit" name="operation" value="Tabung">Volume Tabung <br> V = πr²t</button></li>
+                            <li><button type="submit" name="operation" value="Kerucut">Luas Permukaan Kerucut <br> L = (π r s) + (π r²)</button></li>
+                            <li><button type="submit" name="operation" value="Bola">Luas Permukaan Bola <br> L = 4πr²</button></li>
                         </ul>
                     </div>
-                    <button class="buttonclear" type="submit" name="clear" value="true">Hapus Hasil</button>
+                    <button class="buttonclear" type="submit" name="clear" value="true">HAPUS</button>
                 </form>
 
-                <div class="output-box">
+                <div class="output-bo×">
                     <?php
                     if (isset($_POST['clear'])) {
                         echo "";
@@ -43,22 +46,22 @@
 
                         if (isset($_POST['operation'])) {
                             if ($_POST['operation'] == 'Kubus') {
-                                echo "Luas Permukaan Kubus: " . (6 * $num1 * $num2);
+                                echo "<div class='result'>Luas Permukaan Kubus: " . (6 * $num1 * $num1) . "</div>";
                             } elseif ($_POST['operation'] == 'Balok') {
-                                echo "Luas Permukaan Balok: " . ((2 * $num1 * $num2) + (2 * $num1 * $num3) + (2 * $num2 * $num3));
+                                echo "<div class='result'>Luas Permukaan Balok: " . ((2 * $num1 * $num2) + (2 * $num1 * $num3) + (2 * $num2 * $num3)) . "</div>";
                             } elseif ($_POST['operation'] == 'Prisma_Segitiga') {
                                 $luas_alas = 0.5 * $num1 * $num2;
-                                echo "Luas alas Prisma: " . $luas_alas . "<br>";
+                                echo "<div class='result'>Luas alas Prisma: " . $luas_alas . "<br>" . "</div>";
                                 $volume = $luas_alas * $num3;
-                                echo "Volume Prisma: " . $volume;
+                                echo "<div class='result'>Volume Prisma: " . $volume . "</div>";
                             } elseif ($_POST['operation'] == 'Limas_Segiempat') {
-                                echo "Volume Limas: " . (1 / 3 * $num1 * $num1 * $num2);
+                                echo "<div class='result'>Volume Limas: " . (1 / 3 * ($num1 * $num2) * $num3) . "</div>";
                             } elseif ($_POST['operation'] == 'Tabung') {
-                                echo "Volume Tabung: " . (3.14 * $num1 * $num1 * $num2);
+                                echo "<div class='result'>Volume Tabung: " . (3.14 * $num1 * $num1 * $num2) . "</div>";
                             } elseif ($_POST['operation'] == 'Kerucut') {
-                                echo "Luas Permukaan Kerucut: " . ((3.14 * $num1 * $num1) + (3.14 * $num1 * $num2));
+                                echo "<div class='result'>Luas Permukaan Kerucut: " . ((3.14 * $num1 * $num1) + (3.14 * $num1 * $num2)) . "</div>";
                             } elseif ($_POST['operation'] == 'Bola') {
-                                echo "Luas Permukaan Bola: " . (4 * 3.14 * $num1 * $num1);
+                                echo "<div class='result'>Luas Permukaan Bola: " . (4 * 3.14 * $num1 * $num1) . "</div>";
                             }
                         }
                     }
